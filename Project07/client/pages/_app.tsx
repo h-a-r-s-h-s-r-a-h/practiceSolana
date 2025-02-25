c9b7fb2115b3f8c82a563b71496d152f16c3932c
Harsh <harshvirat894@gmail.com>
2025-02-26 05:09:31 +0530

    Solana

--- Project07/client/pages/_app.tsx
+++ Project07/client/pages/_app.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./globals.css"; 
 import type { AppProps } from "next/app";
-import "./globals.css";
+import WalletContextProvider from "../components/WalletContextProvider";
+import { AppBar } from "../components/AppBar";
+import { SolanaProvider } from "../contexts/SolanaContext";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WalletContextProvider>
+      <SolanaProvider>
+        <div className="min-h-screen bg-gray-900 text-white">
+          <AppBar />
+          <main className="container mx-auto px-4 py-6">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </SolanaProvider>
+    </WalletContextProvider>
+  );
 }
+
+export default MyApp;
