1a7ef6a1129ef0963ac8feadf4e4d96eec8d02fa
Harsh <harshvirat894@gmail.com>
2025-03-12 23:26:08 +0530

    Solana

---
+++ Project10/components/WalletContextProvider.tsx
@@ -0,0 +1,31 @@
+"use-client";
+import { FC, ReactNode, useState, useEffect } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import * as web3 from "@solana/web3.js";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  const wallets = [new PhantomWalletAdapter()];
+  const endpoint = web3.clusterApiUrl("devnet");
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
