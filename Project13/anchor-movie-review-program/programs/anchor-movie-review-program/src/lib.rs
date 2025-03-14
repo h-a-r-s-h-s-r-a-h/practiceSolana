a51b67c654f9c8bb8de9da8896e585dcfd1f479d
Harsh <harshvirat894@gmail.com>
2025-03-15 01:35:34 +0530

    Solana

--- Project13/anchor-movie-review-program/programs/anchor-movie-review-program/src/lib.rs
+++ Project13/anchor-movie-review-program/programs/anchor-movie-review-program/src/lib.rs
@@ -59,6 +59,38 @@ pub mod anchor_movie_review_program {
 
         Ok(())
     }
+
+    pub fn update_movie_review(
+        ctx: Context<UpdateMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        require!(
+            rating >= MIN_RATING && rating <= MAX_RATING,
+            MovieReviewError::InvalidRating
+        );
+
+        require!(
+            description.len() <= MAX_DESCRIPTION_LENGTH,
+            MovieReviewError::DescriptionTooLong
+        );
+
+        msg!("Movie review account space reallocated");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.description = description;
+        movie_review.rating = rating;
+        Ok(())
+    }
+
+    pub fn initialize_token_mint(_ctx: Context<InitializeMint>) -> Result<()> {
+        msg!("Token mint initialized");
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
