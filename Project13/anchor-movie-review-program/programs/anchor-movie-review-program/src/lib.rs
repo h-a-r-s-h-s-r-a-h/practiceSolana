587fd46b93c20d2d77538dd30c80d73084e4d9d7
Harsh <harshvirat894@gmail.com>
2025-03-15 01:39:26 +0530

    Solana

--- Project13/anchor-movie-review-program/programs/anchor-movie-review-program/src/lib.rs
+++ Project13/anchor-movie-review-program/programs/anchor-movie-review-program/src/lib.rs
@@ -87,6 +87,11 @@ pub mod anchor_movie_review_program {
         Ok(())
     }
 
+    pub fn delete_movie_review(_ctx: Context<DeleteMovieReview>, title: String) -> Result<()> {
+        msg!("Movie review for {} deleted", title);
+        Ok(())
+    }
+
     pub fn initialize_token_mint(_ctx: Context<InitializeMint>) -> Result<()> {
         msg!("Token mint initialized");
         Ok(())
@@ -132,6 +137,21 @@ pub struct UpdateMovieReview<'info> {
         mut,
         seeds=[title.as_bytes(),initializer.key().as_ref()],
         bump,
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title:String)]
+pub struct DeleteMovieReview<'info> {
+    #[account(
+        mut,
+        seeds=[title.as_bytes(),initializer.key().as_ref()],
+        bump,
+        close=initializer
     )]
     pub movie_review: Account<'info, MovieAccountState>,
     #[account(mut)]
