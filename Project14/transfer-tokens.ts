fecc1f25132a98943c355316add1f1c7327d26d4
Harsh <harshvirat894@gmail.com>
2025-03-24 04:36:45 +0530

    Solana

--- Project14/transfer-tokens.ts
+++ Project14/transfer-tokens.ts
@@ -1,2 +1,64 @@
 import * as dotenv from "dotenv";
 import * as web3 from "@solana/web3.js";
+import {
+  getKeypairFromEnvironment,
+  getExplorerLink,
+} from "@solana-developers/helpers";
+import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+
+dotenv.config();
+
+const sender = getKeypairFromEnvironment("SECRET_KEY");
+const connection = new web3.Connection(
+  web3.clusterApiUrl("devnet"),
+  "confirmed"
+);
+console.log(
+  `✅ Connection successfully established with sender: `,
+  sender.publicKey.toBase58()
+);
+
+// Add the receiver public key here.
+const recipient = new web3.PublicKey(
+  "AyRNWtd8vEVwebC5zaTNqYMWH1b2tpfGMAXtx5hqtSf5"
+);
+
+// Substitute in your token mint account
+const tokenMintAccount = new web3.PublicKey(
+  "HXW6dELaG4nhfC5WTFwfxZ85dagiqG7YUkLiEdaXFnGp"
+);
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 2);
+
+console.log(`💸 Attempting to send 10 token to ${recipient.toBase58()}...`);
+
+// Get or create the source token account to store this token
+const sourceTokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  sender,
+  tokenMintAccount,
+  sender.publicKey
+);
+
+// Get or create the destination token account to store this token
+const destinationTokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  sender,
+  tokenMintAccount,
+  recipient
+);
+
+// Transfer the tokens
+const signature = await transfer(
+  connection,
+  sender,
+  sourceTokenAccount.address,
+  destinationTokenAccount.address,
+  sender,
+  10 * MINOR_UNITS_PER_MAJOR_UNITS
+);
+
+const explorerLink = getExplorerLink("transaction", signature, "devnet");
+
+console.log(`✅ Transaction confirmed, explorer link is: ${explorerLink}`);
